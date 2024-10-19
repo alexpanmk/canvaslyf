c369e7077a5e9d8d7755505f1c8fadf1c6c19b95
alexpanmk <alexpanmk@gmail.com>
2024-10-19 19:01:37 +0800

    setup firebase and initial auth flow

--- canvaslyfweb/src/App.tsx
+++ canvaslyfweb/src/App.tsx
@@ -1,36 +1,41 @@
 import { useState, useEffect, useRef } from 'react'
 import { AppShell, Group, Stack, Text, TextInput } from '@mantine/core'
-import { Unity, useUnityContext } from "react-unity-webgl";
+// import { Unity, useUnityContext } from "react-unity-webgl";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+import SignUpFlow from './Components/SignUpFlow/SignUpFlow';
+import { getFirestore } from "firebase/firestore";
+
+//Firebase Config
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 function App() {
-  const inputRef = useRef(null);
-  const unityRef = useRef(null);
+  // const inputRef = useRef(null);
+  // const unityRef = useRef(null);
 
-  const [value, setValue] = useState('');
+  // const [value, setValue] = useState('');
 
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "/assets/canvasunity/Build/canvasunity.loader.js",
-    dataUrl: "/assets/canvasunity/Build/canvasunity.data.unityweb",
-    frameworkUrl: "/assets/canvasunity/Build/canvasunity.framework.js.unityweb",
-    codeUrl: "/assets/canvasunity/Build/canvasunity.wasm.unityweb",
-  });
+  // const { unityProvider } = useUnityContext({
+  //   loaderUrl: "/assets/canvasunity/Build/canvasunity.loader.js",
+  //   dataUrl: "/assets/canvasunity/Build/canvasunity.data.unityweb",
+  //   frameworkUrl: "/assets/canvasunity/Build/canvasunity.framework.js.unityweb",
+  //   codeUrl: "/assets/canvasunity/Build/canvasunity.wasm.unityweb",
+  // });
 
-  useEffect(() => {
-    // const handleEsc = (event: KeyboardEvent) => {
-    //   if (event.key === 'Escape') {
-    //     console.log('Escape key pressed');
-    //     unityRef.current.blur();
-    //     inputRef.current.focus();
-
-    //   }
-    // };
-
-    // window.addEventListener('keydown', handleEsc);
-
-    return () => {
-      // window.removeEventListener('keydown', handleEsc);
-    };
-  }, []);
 
   return (
     <>
@@ -38,21 +43,19 @@ function App() {
       <AppShell>
 
         <AppShell.Main style={{ height: '100vh', position: 'relative', overflow: 'hidden' }}>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
 
-          {/* <Unity ref={unityRef} unityProvider={unityProvider} style={{ width: '100%', height: '100%' }} /> */}
-          <div style={{
-            backgroundImage: 'url(/assets/images/galleryplaceholder.png)',
-            backgroundSize: 'cover',
-            position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1
-          }}>
-            <Group style={{ width: '100%', height: '100%' }}>
-              {/* <TextInput onClick={() => {
-                console.log('clicked');
-              }} value={value} onChange={(evt) => {
-                setValue(evt.currentTarget.value);
-              }} ref={inputRef} placeholder="Search" style={{ width: '100%' }} /> */}
+          <SignedIn>
+            <Group style={{
+              backgroundImage: 'url(/assets/images/galleryplaceholder.png)',
+              backgroundSize: 'cover',
+              width: '100%', height: '100%'
+            }}>
+              <SignUpFlow signUpType={'artist'} />
             </Group>
-          </div>
+          </SignedIn>
         </AppShell.Main>
       </AppShell>
 
