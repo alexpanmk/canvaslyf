68a1f0028e426c04a2660783286c8c4f415f7da0
alexpanmk <alexpanmk@gmail.com>
2024-10-25 19:13:35 +0800

    Refactor PaintingProximity script to pass canvas as a string parameter in nearCanvas event

--- canvaslyfunity/Assets/PaintingProximity.cs
+++ canvaslyfunity/Assets/PaintingProximity.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 public class PaintingProximity : MonoBehaviour
 {
     [DllImport("__Internal")]
-    private static extern void nearCanvas(string value);
+    private static extern void nearCanvas(string canvas);
 
 
 
@@ -21,13 +21,7 @@ public class PaintingProximity : MonoBehaviour
 
     void OnPlayerNearPainting()
     {
-        if (transform.parent != null)
-        {
-            // Log the parent's name
-            nearCanvas(gameObject.name);
-            // Debug.Log("Parent object name: " + gameObject.name);
-        }
-
+        nearCanvas(gameObject.name); // Call the JavaScript function with the painting's name
 
         // Trigger the event you want, e.g., display a UI element or interact with the painting
         Debug.Log("Player is near the painting!");
