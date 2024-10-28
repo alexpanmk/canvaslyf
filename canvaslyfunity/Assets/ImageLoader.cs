b0561365748b9d55c93a4e24c8bc75a1757aca60
alexpanmk <alexpanmk@gmail.com>
2024-10-28 23:46:54 +0800

    Refactor ImageLoader.cs to update image URLs

--- canvaslyfunity/Assets/ImageLoader.cs
+++ canvaslyfunity/Assets/ImageLoader.cs
@@ -28,7 +28,7 @@ public class ImageLoader : MonoBehaviour
         string imageUrl2 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2FvBuksMdG2hsMrkI6GUfk%2F5bde999c8d3f49639100b5b795b730d2.jpg?alt=media&token=2efcf800-227d-4971-aceb-5ff22fc0a483";
         string imageUrl3 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2FIFafPhHfmjs0DT1hrlxZ%2Fca6303745e4841c8b2dd81e63b9d6875.jpg?alt=media&token=42bc2d3d-9a3a-4e52-ae1f-822646ba13d9";
         string imageUrl4 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2FM0qeelYoMtrwi48k1sK7%2F8ec6ad333d05471082b853b8887f40eb.jpg?alt=media&token=17306ce2-7cc3-49c8-aff8-21072dc0574c";
-        string imageUrl5 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2Fi2V2dwjx0KltxJYASIF8%2F72573a62a2e44dc2a273a68c63fe6b14.jpg?alt=media&token=d63b297c-cb36-4825-a7c2-c88127dace87";
+        string imageUrl5 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2F1rTrKb08qQmIHWq67GU0%2Fe2b91723fdba48499cf731e727c439ba.jpeg?alt=media&token=99439556-2e08-4b29-8da6-f4bb62326e3e";
         string imageUrl6 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2Fxz8LDaudtVXlq5lQBzS1%2F90e4230f61c64c559c7fc7ed0fa162db.jpg?alt=media&token=e087ac41-3808-402f-9414-d05ca78101c8";
         string imageUrl7 = "https://firebasestorage.googleapis.com/v0/b/canvaslyf.appspot.com/o/artworks%2FFJgqSnKG9hGamTOaaxUT%2Fhealingland.jpg.png?alt=media&token=e1ab41c0-6dab-46c1-b9bd-8bc0f5dd2760";
 
